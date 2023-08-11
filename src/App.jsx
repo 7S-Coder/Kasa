5ed2da15abbb4eada1770aabe4f3c77fa7d51792
7S-Coder <dylan.e33@hotmail.fr>
2023-08-11 14:49:28 +0200

    separate routes, put in a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Error404 from "./pages/Error404";
+import { BrowserRouter } from "react-router-dom";
+import RoutesConfig from "./routes/RoutesConfig";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
+      <RoutesConfig />
     </BrowserRouter>
   );
 }
